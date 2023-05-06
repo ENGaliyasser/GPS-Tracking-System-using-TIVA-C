3d2d79d5f2aef25ee06a6e0c6d05e11b360891e8
Donia Hany <doniahanyy312@gmai.com>
2023-05-06 18:25:45 +0300

    Uploaded GPS.c and GPS.h

---
+++ GPS project/HAL/GPS/GPS.h
@@ -0,0 +1,23 @@
+#ifndef GPS_H_INCLUDED
+#define GPS_H_INCLUDED
+
+int readGPS(char * lat, char * log);   ///if it returns 0 then its working and 1 means didnt find gprmc and -1 means invalid data
+
+double calcDistance(volatile double latitude_1, volatile double logitude_1, volatile double latitude_2, volatile double logitude_2);
+
+
+//double floatVal(char *c , int latOrlog);
+
+double floatVal(char *c);
+
+double convertCoords(char * old);
+
+double degToRad(double deg);
+
+double calcDistance(volatile double latitude_1, volatile double logitude_1, volatile double latitude_2, volatile double logitude_2);
+
+
+void readGPSDouble(volatile double * dlat,volatile double * dlog);
+
+
+#endif // GPS_H_INCLUDED
