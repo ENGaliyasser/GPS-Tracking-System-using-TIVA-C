b02853a2c1e3f8a36f8ff096b2eb4a22aa20d4a2
ali yasser ali <gn3li2002@gmail.com>
2023-05-07 17:34:26 +0300

    .

--- GPS project/Application/APP.c
+++ GPS project/Application/APP.c
@@ -8,7 +8,8 @@ UART_Init( 1 , 9600 );
 //void UART_StringTX(char num ,char *str);
 //void UART_StringRX(char num ,char *str, char stopCh);
 	while(1){
-		UART_CharTX(1 , 'a');
+		char x = UART_CharRX(1);
+		UART_CharTX(1 , x);
 		
 	}
 		
