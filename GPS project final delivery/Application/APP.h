d4fd70ad330ee8078ad4579ee39e158d3e1845f4
ali yasser ali <113934823+ENGaliyasser@users.noreply.github.com>
2023-05-13 15:39:40 +0300

    Update APP.h
    ..

---
+++ GPS project final delivery/Application/APP.h
@@ -0,0 +1,19 @@
+#ifndef APP_H_
+#define APP_H_
+
+#include "../../Utilities/bitManipulation.h"
+#include "../../Utilities/tm4c123gh6pm.h"
+#include "../../MCAL/GPIO/GPIO.h"
+#include "../../MCAL/UART/UART.h"
+#include "../../MCAL/SYSTICK/Systick.h"
+//#include "../MCAL/sysctl/sysctl.h"
+//#include "../MCAL/sysctl/hw_ints.h"
+//#include "driverlib/sysctl.h"
+//#include "driverlib/inc/hw_ints.h"
+#include "../../HAL/GPS/GPS.h"
+#include "../../HAL/LED/LED.h"
+#include "stdint.h"
+
+
+
+#endif
